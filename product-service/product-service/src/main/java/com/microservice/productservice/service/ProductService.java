24499bd88c24dfe67e71fedfee9d430f4014d7b4
Siddhant Vyas <mailsiddhantvyas@gmail.com>
2023-03-19 11:59:57 +0530

    Service created

---
+++ product-service/product-service/src/main/java/com/microservice/productservice/service/ProductService.java
@@ -0,0 +1,28 @@
+package com.microservice.productservice.service;
+
+import com.microservice.productservice.dto.ProductRequest;
+import com.microservice.productservice.model.Product;
+import com.microservice.productservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public void createProduct(ProductRequest productRequest){
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+
+        productRepository.save(product);
+        log.info("Product {} is saved",product.getId());
+    }
+}
