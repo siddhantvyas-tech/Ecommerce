7ecd9d3322876e7728369258399cba673a76a69a
Siddhant Vyas <mailsiddhantvyas@gmail.com>
2023-03-19 15:39:50 +0530

    Order Service Complete

---
+++ order-service/order-service/src/main/java/com/microservice/orderservice/model/OrderLineItems.java
@@ -0,0 +1,28 @@
+package com.microservice.orderservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "t_order_line_items")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderLineItems {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private BigDecimal price;
+    private Integer quantity;
+
+//    @ManyToOne
+//    @JoinColumn(name = "order_id")
+//    private Order order;
+
+}
