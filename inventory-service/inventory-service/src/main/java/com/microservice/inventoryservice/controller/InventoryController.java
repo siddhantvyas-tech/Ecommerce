bf7e7fb4cdfe058188c3933e5a773b60aa114fee
Siddhant Vyas <mailsiddhantvyas@gmail.com>
2023-04-01 17:27:52 +0530

    Inventory Service Complete with interprocess call from order service

--- inventory-service/inventory-service/src/main/java/com/microservice/inventoryservice/controller/InventoryController.java
+++ inventory-service/inventory-service/src/main/java/com/microservice/inventoryservice/controller/InventoryController.java
@@ -1,10 +1,13 @@
 package com.microservice.inventoryservice.controller;
 
 import com.microservice.inventoryservice.dto.InventoryRequest;
+import com.microservice.inventoryservice.dto.InventoryResponse;
 import com.microservice.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
@@ -13,13 +16,13 @@ public class InventoryController {
 
     private final InventoryService inventoryService;
 
-    @GetMapping("/{sku-code}")
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku-code") String skuCode){
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode,@RequestParam List<Integer> quantity){
         return inventoryService.isInStock(skuCode);
     }
 
-    @PostMapping
+    @PostMapping("/add-inventory")
     @ResponseStatus(HttpStatus.CREATED)
     public void saveInventory(@RequestBody InventoryRequest inventoryRequest){
         inventoryService.saveInventory(inventoryRequest);
--- inventory-service/inventory-service/src/main/java/com/microservice/inventoryservice/service/InventoryService.java
+++ inventory-service/inventory-service/src/main/java/com/microservice/inventoryservice/service/InventoryService.java
@@ -1,12 +1,15 @@
 package com.microservice.inventoryservice.service;
 
 import com.microservice.inventoryservice.dto.InventoryRequest;
+import com.microservice.inventoryservice.dto.InventoryResponse;
 import com.microservice.inventoryservice.model.Inventory;
 import com.microservice.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,8 +19,14 @@ public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode){
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public List<InventoryResponse> isInStock(List<String> skuCode){
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(inventory ->
+                    InventoryResponse.builder()
+                        .skuCode(inventory.getSkuCode())
+                        .isInStock(inventory.getQuantity()>0)
+                        .build()
+                ).toList();
     }
 
     public void saveInventory(InventoryRequest inventoryRequest) {
@@ -25,7 +34,6 @@ public class InventoryService {
                 .quantity(inventoryRequest.getQuantity())
                 .skuCode(inventoryRequest.getSkuCode()).build();
         inventoryRepository.save(inventory);
-
         log.info("Inventory add {}",inventory.getId());
     }
 }
